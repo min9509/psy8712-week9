38eeef766b961f53cdbb06ff8aa657e8c5cd1ca7
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-03-24 22:20:37 -0500

    Part 2 - Objective 4-3

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,10 +1,10 @@
-# Script Settings and Resources
+#### Script Settings and Resources #### 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(httr)
 library(jsonlite)
 
-## Data Import and Cleaning
+#### Data Import and Cleaning #### 
 # Import data from Reddit by using JSON
 url <- "https://www.reddit.com/r/rstats.json"
 rstats_data <- fromJSON(url)
@@ -16,13 +16,18 @@ rstats_tbl <- rstats_data$data$children %>%
     comments = data$num_comments) %>%
   select(post, upvotes, comments)
 
-## Visualization
+#### Visualization #### 
 # Visualize the relationship between upvotes and comments
 ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
   geom_point() +
   geom_smooth(method = "lm") +
   labs(x = "Number of Upvotes", y = "Number of Comments", title = "The Scatterplot between Upvotes and Comments")
 
-
-
-?fromJSON
+#### Analysis #### 
+# Analyze the correlation coefficient and p-value by using cor.test
+cor_test <- cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
+cor_test_coefficient <- cor_test$estimate
+cor_test_p <- cor_test$p.value
+cor_test
+cor_test_coefficient
+cor_test_p
