fdf56097da052b84c2eca8d97c3d9e9f65226f64
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-03-24 23:43:12 -0500

    Compress some lines.

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,13 +1,11 @@
 #### Script Settings and Resources #### 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-library(httr)
 library(jsonlite)
 
 #### Data Import and Cleaning #### 
 # Import data from Reddit by using JSON
-url <- "https://www.reddit.com/r/rstats.json"
-rstats_data <- fromJSON(url)
+rstats_data <- fromJSON("https://www.reddit.com/r/rstats.json")
 
 # Creating rstats_tbl by using mutate and select function
 rstats_tbl <- rstats_data$data$children %>%
