f13cb45a3db4f57c973395b75a2487508a3a91e1
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-03-24 22:12:59 -0500

    Part 2 - Object 4-2

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(httr)
 library(jsonlite)
 
-# Data Import and Cleaning
+## Data Import and Cleaning
 # Import data from Reddit by using JSON
 url <- "https://www.reddit.com/r/rstats.json"
 rstats_data <- fromJSON(url)
@@ -15,6 +15,14 @@ rstats_tbl <- rstats_data$data$children %>%
     upvotes = data$ups,
     comments = data$num_comments) %>%
   select(post, upvotes, comments)
+
+## Visualization
+# Visualize the relationship between upvotes and comments
+ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  labs(x = "Number of Upvotes", y = "Number of Comments", title = "The Scatterplot between Upvotes and Comments")
+
 
 
 ?fromJSON
